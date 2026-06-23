cf0a05f5479a2b73b3f4c3bd8a82628de228dd5d
dennislacara <lacaradennis@gmail.com>
2026-06-23 14:25:12 +0200

    a

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ def main(page: ft.Page):
     my_model = Model()
     my_view = View(page)
     my_controller = Controller(my_view, my_model)
+
     my_view.set_controller(my_controller)
     my_view.load_interface()
 
